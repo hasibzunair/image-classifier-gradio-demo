df67541a5b80f16c8360c623497b18607d96a9b8
hasibzunair <hasibzunair@gmail.com>
2022-05-11 14:51:23 -0400

    update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def inference(input_image):
 inputs = gr.inputs.Image(type='pil')
 outputs = gr.outputs.Label(type="confidences",num_top_classes=5)
 
-title = "An Image Classification Demo with ResNet"
+title = "Image Recognition using ResNet"
 description = "Demo of a ResNet image classifier trained on the ImageNet dataset. To use it, simply upload your image, or click one of the examples to load them. Read more at the links below."
 article = "<p style='text-align: center'><a href='https://arxiv.org/abs/1512.03385' target='_blank'>Deep Residual Learning for Image Recognition</a> | <a href='https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py' target='_blank'>Github Repo</a></p>"
 
@@ -65,37 +65,3 @@ gr.Interface(inference,
             article=article,
             analytics_enabled=False).launch()
 
-# import torch
-# import requests
-# import gradio as gr
-
-# from torchvision import transforms
-
-# """
-# Built following https://www.gradio.app/image_classification_in_pytorch/.
-# """
-
-# # Load model
-# model = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', pretrained=True).eval()
-
-# # Download human-readable labels for ImageNet.
-# response = requests.get("https://git.io/JJkYN")
-# labels = response.text.split("\n")
-
-# def predict(inp):
-#   inp = transforms.ToTensor()(inp).unsqueeze(0)
-#   with torch.no_grad():
-#     prediction = torch.nn.functional.softmax(model(inp)[0], dim=0)
-#     confidences = {labels[i]: float(prediction[i]) for i in range(1000)}    
-#   return confidences
-
-# title = "Image Classifier"
-
-# article = "<p style='text-align: center'><a href='https://arxiv.org/abs/1512.03385' target='_blank'>Deep Residual Learning for Image Recognition</a> | <a href='https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py' target='_blank'>Github Repo</a></p>"
-
-# gr.Interface(fn=predict, 
-#              inputs=gr.inputs.Image(type="pil"),
-#              outputs=gr.outputs.Label(num_top_classes=3),
-#              examples=["example1.jpg", "example2.jpg"],
-#              theme="default",
-#              css=".footer{display:none !important}").launch()
