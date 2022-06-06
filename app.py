f9a5b44e27cdff4410c9c0f85281486f19920719
hasibzunair <hasibzunair@gmail.com>
2022-06-06 11:54:52 -0400

    cleanup

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import os
 import torch
+import gradio as gr
+
 from PIL import Image
 from torchvision import transforms
-import gradio as gr
-import os
 
 
 """
@@ -11,14 +12,17 @@ https://huggingface.co/spaces/pytorch/ResNet/tree/main
 https://www.gradio.app/image_classification_in_pytorch/
 """
 
+# Get classes list
 os.system("wget https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt")
 
+# Load PyTorch model
 model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
 model.eval()
 
 # Download an example image from the pytorch website
 torch.hub.download_url_to_file("https://github.com/pytorch/hub/raw/master/images/dog.jpg", "dog.jpg")
 
+# Inference!
 def inference(input_image):
     preprocess = transforms.Compose([
         transforms.Resize(256),
@@ -29,7 +33,7 @@ def inference(input_image):
     input_tensor = preprocess(input_image)
     input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
 
-    # move the input and model to GPU for speed if available
+    # Move the input and model to GPU for speed if available
     if torch.cuda.is_available():
         input_batch = input_batch.to('cuda')
         model.to('cuda')
@@ -49,13 +53,16 @@ def inference(input_image):
         result[categories[top5_catid[i]]] = top5_prob[i].item()
     return result
 
+# Define ins outs placeholders
 inputs = gr.inputs.Image(type='pil')
 outputs = gr.outputs.Label(type="confidences",num_top_classes=5)
 
+# Define style
 title = "Image Recognition Demo"
 description = "This is a prototype application which demonstrates how artifical intelligence based systems can recognize what object(s) is present in an image. This fundamental task in computer vision known as `Image Classification` has applications stretching from autonomous vehicles to medical imaging. To use it, simply upload your image, or click one of the examples images to load them, which I took at Montréal Biodôme! Read more at the links below."
 article = "<p style='text-align: center'><a href='https://arxiv.org/abs/1512.03385' target='_blank'>Deep Residual Learning for Image Recognition</a> | <a href='https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py' target='_blank'>Github Repo</a></p>"
 
+# Run inference
 gr.Interface(inference, 
             inputs, 
             outputs, 
